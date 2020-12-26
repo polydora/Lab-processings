38e2fc101a19d594178e7b5a59188935bd30c3dd
Vadim Khaitov <polydora@rambler.ru>
2020-12-26 22:24:47 +0300

    заява генератор

--- Официальные группы/Zajava_generator/zajava_generator.R
+++ Официальные группы/Zajava_generator/zajava_generator.R
@@ -1,10 +1,12 @@
 library(stringdist)
 library(readxl)
 library(dplyr)
+library(reshape2)
 
+library(knitr)
+library(markdown)
+library(rmarkdown)
 
-
-stringdist("ca","abc",method="cosine")
 
 
 registered <- read_excel("Data/ebc_distant_agreement.xlsx")
@@ -25,6 +27,27 @@ Dist_names <- as.data.frame(t(d))
 
 which(d < 0.2)
 
-Dist_names < 0.06
+Dist_names < 0.1
 
-Dist_names$`Коррой Алексей`)
+found_in_registered <- melt(d) %>% filter(value < 0.09) %>% group_by(Var1) %>% summarize(Dist = mean(value))
+
+
+lmbe_not_in_registered <- lmbe_group %>% filter(!(paste(Ch_Fam, Ch_Name) %in% as.character(found_in_registered$Var1)))
+
+
+
+for (i in unique(rownames(lmbe_not_in_registered))){
+  rmarkdown::render('Zajava.Rmd',  # file 2
+                    output_file =  paste(lmbe_not_in_registered$Ch_Fam[i],".html", sep=''), 
+                    output_dir = '/Zajaves')
+  
+  # for pdf reports  
+  #   rmarkdown::render(input = "/Users/majerus/Desktop/R/auto_reporting/test/r_script_pdf.Rmd", 
+  #           output_format = "pdf_document",
+  #           output_file = paste("test_report_", car, Sys.Date(), ".pdf", sep=''),
+  #           output_dir = "/Users/majerus/Desktop/R/auto_reporting/test/reports")
+  
+}
+
+
+
